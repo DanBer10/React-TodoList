1bd23ad5760a6b28fd2a58eca38803059e88e959
DanBer10 <dannebernhardt@outlook.com>
2021-08-17 12:59:45 +0200

    changed card look

--- todofolder/src/App.js
+++ todofolder/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -8,104 +8,129 @@ function App() {
   const [editingText, setEditingText] = useState("");
 
   useEffect(() => {
-    const temp = localStorage.getItem("todos")
-    const loadedTodos = JSON.parse(temp)
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
 
-    if(loadedTodos) {
+    if (loadedTodos) {
       setTodos(loadedTodos);
     }
-  }, [])
+  }, []);
 
-  useEffect (() => {
-    const temp = JSON.stringify(todos)
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
     localStorage.setItem("todos", temp);
-  }, [todos])
-
-
+  }, [todos]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const newTodo = {
       id: new Date().getTime(),
       text: todo,
       completed: false,
-    }
+    };
     if (newTodo.text !== "") {
-      setTodos([...todos].concat(newTodo))
-      setTodo("")
+      setTodos([...todos].concat(newTodo));
+      setTodo("");
     }
-  }
+  };
 
   function deleteTodo(id) {
-    const updatedTodos = [...todos].filter((todo) => todo.id !== id)
-      setTodos(updatedTodos)
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
   }
 
   function toggleComplete(id) {
     const updatedTodos = [...todos].map((todo) => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
       }
-      return todo
-    })
+      return todo;
+    });
 
-    setTodos(updatedTodos)
+    setTodos(updatedTodos);
   }
 
   function editTodo(id) {
     const updatedTodos = [...todos].map((todo) => {
-      if(todo.id === id) {
-        todo.text = editingText
+      if (todo.id === id) {
+        todo.text = editingText;
       }
-      return todo
-    })
-    setTodos(updatedTodos)
-    setTodoEditing(null)
-    setEditingText("")
+      return todo;
+    });
+    setTodos(updatedTodos);
+    setTodoEditing(null);
+    setEditingText("");
   }
 
   return (
     <div className="App">
+      <h1 className="head-title">TodoList created in ReactJS</h1>
       <div className="inner-container">
-      <form onSubmit={handleSubmit} className="form-container">
-        <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} className="input-field"  maxlength="25"/>
-        <button className="btn add">Add todo</button>
-      </form>
-      {todos.map((todo) =>  <div key={todo.id}>
-
-      {todoEditing === todo.id ?
-      (<input type="text" onChange={(e) => setEditingText(e.target.value)} value={editingText} maxlength="25" placeholder="Edit text and submit" className="input-field submit-change"/>)
-       : 
-      (<div className="card">
-        <div className="card-text">
-          <div>
-          {todo.text}
-          </div>
-        <input type="checkbox" 
-          onChange={() => 
-          toggleComplete(todo.id)}
-          checked={todo.completed}
-          className="checkboxen"
-          maxlength="25"
+        <form onSubmit={handleSubmit} className="form-container">
+          <input
+            type="text"
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+            className="input-field"
+            maxlength="25"
           />
+          <button className="btn add">Add todo</button>
+        </form>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            {todoEditing === todo.id ? (
+              <input
+                type="text"
+                onChange={(e) => setEditingText(e.target.value)}
+                value={editingText}
+                maxlength="25"
+                placeholder="Edit text and submit"
+                className="input-field submit-change"
+              />
+            ) : (
+              <div className="card">
+                <div className="card-text">
+                  <div>{todo.text}</div>
+                  <input
+                    type="checkbox"
+                    onChange={() => toggleComplete(todo.id)}
+                    checked={todo.completed}
+                    className="checkboxen"
+                    maxlength="25"
+                  />
+                </div>
+              </div>
+            )}
+
+            <div className="btn-container">
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn"
+                id="delete"
+              >
+                Delete
+              </button>
+
+              {todoEditing === todo.id ? (
+                <button
+                  onClick={() => editTodo(todo.id)}
+                  className="btn submitEdit"
+                >
+                  Submit Edit
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTodoEditing(todo.id)}
+                  className="btn"
+                  id="edit"
+                >
+                  Edit Todo
+                </button>
+              )}
+            </div>
           </div>
-         
-
-      </div>)}
-
-
-          <div className="btn-container">
-          <button onClick={() => deleteTodo(todo.id)} className="btn" id="delete">Delete</button> 
-         
-           {todoEditing === todo.id ? 
-           (<button onClick={() => editTodo(todo.id)} className="btn submitEdit">Submit Edit</button>)
-            : 
-          (<button onClick={() => setTodoEditing(todo.id)} className="btn" id="edit">Edit Todo</button>)}
-          </div>
-          
-        </div>
-      )}
+        ))}
       </div>
     </div>
   );
